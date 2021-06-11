cd75450f788b92b30ca9989261f49cbda8d8c5d8
Rafael Cavalcanti <rafael240519@gmail.com>
2021-06-11 01:22:22 -0300

    gerado scaffold de encomenda

---
+++ db/migrate/20210611042004_create_encomendas.rb
@@ -0,0 +1,11 @@
+class CreateEncomendas < ActiveRecord::Migration[6.1]
+  def change
+    create_table :encomendas do |t|
+      t.float :peso
+      t.string :remetente
+      t.references :Morador, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
