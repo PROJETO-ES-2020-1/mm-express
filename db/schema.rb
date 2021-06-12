a34da3dffed6a4c1577e471d337e902f31682f40
Joanne Gabriela <joanneegabriela@gmail.com>
2021-06-12 00:10:00 -0300

    Criada referência à entrega externa na tabela de entrega interna

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_224316) do
+ActiveRecord::Schema.define(version: 2021_06_12_030241) do
+
+  create_table "encomendas", force: :cascade do |t|
+    t.float "peso"
+    t.string "remetente"
+    t.integer "Morador_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["Morador_id"], name: "index_encomendas_on_Morador_id"
+  end
+
+  create_table "entrega_externas", force: :cascade do |t|
+    t.date "dataChegada"
+    t.time "horaChegada"
+    t.string "recebidoPor"
+    t.integer "Encomenda_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["Encomenda_id"], name: "index_entrega_externas_on_Encomenda_id"
+  end
 
   create_table "entrega_internas", force: :cascade do |t|
     t.date "data_entrega"
-    t.time "hora_recebina"
+    t.time "hora_recebida"
     t.string "recebido_por"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "entrega_externa_id", null: false
+    t.index ["entrega_externa_id"], name: "index_entrega_internas_on_entrega_externa_id"
   end
 
+  add_foreign_key "encomendas", "Moradors"
+  add_foreign_key "entrega_externas", "Encomendas"
+  add_foreign_key "entrega_internas", "entrega_externas"
 end
