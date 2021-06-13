f1187ae9a868264b42ae8d90da3d1bbbe30b698a
Joanne Gabriela <joanneegabriela@gmail.com>
2021-06-13 02:02:45 -0300

    Atualização nas associations — nested_attributes de Encomenda e EntregaExterna

--- app/models/encomenda.rb
+++ app/models/encomenda.rb
@@ -1,4 +1,6 @@
 class Encomenda < ApplicationRecord
   belongs_to :usuario
   belongs_to :entrega_externa
+
+  accepts_nested_attributes_for :entrega_externa, update_only: true
 end
--- app/models/entrega_externa.rb
+++ app/models/entrega_externa.rb
@@ -1,5 +1,3 @@
 class EntregaExterna < ApplicationRecord
   has_one :encomenda
-
-  accepts_nested_attributes_for :encomenda, update_only: true
 end
