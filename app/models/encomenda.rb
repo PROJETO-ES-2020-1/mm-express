2258b51e79e1dccd829894d692346421d21d5ef1
Rafael Cavalcanti <rafael240519@gmail.com>
2021-06-14 00:34:16 -0300

    validations de encomenda

--- app/models/encomenda.rb
+++ app/models/encomenda.rb
@@ -1,4 +1,7 @@
 class Encomenda < ApplicationRecord
   belongs_to :usuario
   belongs_to :entrega_externa
+
+  validates :peso, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :remetente, presence: true, length: {minimum: 3}
 end
