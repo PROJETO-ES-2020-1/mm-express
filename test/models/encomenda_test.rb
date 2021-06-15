e0e577ce0bf1632f13693e6d620e519ee4137890
Rafael Cavalcanti <rafael240519@gmail.com>
2021-06-14 22:14:02 -0300

    correcao dos teste de model de encomenda e adicao de um novo teste

--- test/models/encomenda_test.rb
+++ test/models/encomenda_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class EncomendaTest < ActiveSupport::TestCase
 
   test "nao deve criar uma encomenda com remetende com menos de 3 caracteres" do
-    encomenda = Encomenda.new peso: 1.1, remetente: "F", usuario: @usuario = usuarios(:one), entrega_externa: @entega_externa = entrega_externas(:one)
+    encomenda = Encomenda.new(peso: 1.1, remetente: "F", usuario: @usuario = usuarios(:one), entrega_externa: @entega_externa = entrega_externas(:one))
     assert_not encomenda.save
   end
 
@@ -13,8 +13,13 @@ class EncomendaTest < ActiveSupport::TestCase
   end
 
   test "nao deve criar um ecomenda com peso negativo" do
-    encomenda = Encomenda.new peso: -1.1, remetente: "FAB", usuario: @usuario = usuarios(:one), entrega_externa: @entega_externa = entrega_externas(:one)
+    encomenda = Encomenda.new(peso: -1.1, remetente: "FAB", usuario: @usuario = usuarios(:one), entrega_externa: @entega_externa = entrega_externas(:one))
     assert_not encomenda.save
+  end
+
+  test "deve criar um ecomenda corretamente" do
+    encomenda = Encomenda.new(peso: 1.1, remetente: "FAB", usuario: @usuario = usuarios(:one), entrega_externa: @entega_externa = entrega_externas(:one))
+    assert encomenda.save
   end
 
 end
