7e0f1064e4792a20c763f38ad6f1cd48210da952
Rafael Cavalcanti <rafael240519@gmail.com>
2021-06-14 21:54:52 -0300

    teste de model entrega_externa

--- test/models/entrega_externa_test.rb
+++ test/models/entrega_externa_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class EntregaExternaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "nao deve criar uma entrega externa vazia" do
+    entrega_externa = EntregaExterna.new
+    assert_not entrega_externa.save
+  end
+
+  test "nao deve criar uma entrega externa com recebido_por vazio" do
+    entrega_externa = EntregaExterna.new dataChegada: Date.today, horaChegada: Time.now, recebidoPor: ""
+    assert_not entrega_externa.save
+  end
+
+  test "nao deve criar entrega externa com data posterior a data atual" do
+    entrega_externa = EntregaExterna.new dataChegada: Date.tomorrow, horaChegada: Time.now, recebidoPor: ""
+    assert_not entrega_externa.save
+  end
+
 end
