619790ac77db5ceb3db96d3dd6982a15267bb746
Rafael Cavalcanti <rafael240519@gmail.com>
2021-06-26 17:19:31 -0300

    retornado referencia a entrega externa em solicitacao de repasse

--- db/migrate/20210611042511_create_entrega_externas.rb
+++ db/migrate/20210611042511_create_entrega_externas.rb
@@ -4,7 +4,6 @@ class CreateEntregaExternas < ActiveRecord::Migration[6.1]
       t.date :dataChegada
       t.time :horaChegada
       t.string :recebidoPor
-      t.references :solicitacao_repasse, null: false, foreign_key: true
 
       t.timestamps
     end
