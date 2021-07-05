148aa40f65db207a7cc42fc234155e54facf97a6
Joanne Gabriela <joanneegabriela@gmail.com>
2021-07-05 20:11:41 -0300

    Atualização no routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+
   resources :solicitacao_repasses
   resources :sobreaviso_de_entregas
-  get "sessions/new"
+
   resources :usuarios
+
   resources :entrega_internas
   resources :entrega_externas
   resources :encomendas
-
-  resources :usuarios do
-    resources :encomendas
-    resources :entrega_internas
-  end
 
   get "sign_in" => "sessions#new"
   post "sign_in" => "sessions#create"
